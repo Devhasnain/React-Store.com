669c0582962b4fd871c6c6d09c18bc6546e99e76
Hasnain Alam <hasnainalam1166@gmail.com>
2022-09-18 04:59:18 +0500

    session url changed

--- Routes/Sripe.js
+++ Routes/Sripe.js
@@ -5,24 +5,32 @@ const Sequrity = require('../Utils/Sequrity');
 const router = express.Router();
 
 router.post('/create-checkout-session',Sequrity, Permission, async (req, res) => {
-  const {products}=req.body;
-  const session = await stripe.checkout.sessions.create({
-    line_items:products.map((item)=>{
-      return{
-        price_data:{
-          currency:"usd",
-          product_data:{
-            name:item.title,
+  try {
+    const {products}=req.body;
+    const session = await stripe.checkout.sessions.create({
+      line_items:products.map((item)=>{
+        return{
+          price_data:{
+            currency:"usd",
+            product_data:{
+              name:item.title,
+            },
+            unit_amount:item.price*100
           },
-          unit_amount:item.price*100
-        },
-        quantity:1
-      }
-    }),
-    mode: 'payment',
-    success_url: `${'http://localhost:3000/'}/order_successful'`,
-    cancel_url: `${'http://localhost:3000/'}canceled=true`,
-  });
-  res.send(session.url);
+          quantity:1
+        }
+      }),
+      mode: 'payment',
+      success_url: `${'http://localhost:3000/'}/order_successful'`,
+      cancel_url: `${'http://localhost:3000/'}canceled=true`,
+    });
+    res.status(200).json({url:session.url});  
+  } catch (error) {
+    return res.status(500).json({
+      msg:"Some error occured please tryagain",
+      access:false
+    })
+  }
+  
 });
 module.exports = router;
